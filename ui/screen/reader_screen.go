7fa809e47cdb9b187897df64891bbefc2d40b389
Simon Hostettler <simon.hostettler@protonmail.com>
2025-10-26 19:55:28 +0100

    add viewport component

--- ui/screen/reader_screen.go
+++ ui/screen/reader_screen.go
@@ -1,45 +1,39 @@
 package screen
 
 import (
-	"strings"
-
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"hostettler.dev/dnc/ui/command"
 	styles "hostettler.dev/dnc/ui/util"
+	"hostettler.dev/dnc/ui/viewport"
 	"hostettler.dev/dnc/util"
 )
 
 var readerHeight = 30
 
 type ReaderScreen struct {
-	keymap  util.KeyMap
-	cursor  int
-	content string
+	keymap   util.KeyMap
+	viewport *viewport.Viewport
 }
 
 func NewReaderScreen(keymap util.KeyMap) *ReaderScreen {
-	return &ReaderScreen{keymap, 0, ""}
+	return &ReaderScreen{
+		keymap,
+		viewport.NewViewport(keymap, readerHeight, styles.SmallScreenWidth+2),
+	}
 }
 
 func (s *ReaderScreen) Init() tea.Cmd {
-	return nil
+	return s.viewport.Init()
 }
 
 func (s *ReaderScreen) StartRead(content string) {
-	s.cursor = 0
-	s.content = lipgloss.NewStyle().
-		Width(styles.SmallScreenWidth - 2).
-		Render(content)
-}
-
-func (s *ReaderScreen) MoveCursor(offset int) {
-	newCursor := s.cursor + offset
-
-	if newCursor >= 0 && newCursor+readerHeight < len(s.contentToLines()) {
-		s.cursor = newCursor
-	}
+	s.viewport.Reset()
+	s.viewport.UpdateContent(
+		lipgloss.NewStyle().
+			Width(styles.SmallScreenWidth - 2).
+			Render(content))
 }
 
 func (s *ReaderScreen) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -48,32 +42,20 @@ func (s *ReaderScreen) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch {
-		case key.Matches(msg, s.keymap.Up):
-			s.MoveCursor(-1)
-		case key.Matches(msg, s.keymap.Down, s.keymap.Enter):
-			s.MoveCursor(1)
 		case key.Matches(msg, s.keymap.Escape) || key.Matches(msg, s.keymap.Show):
 			cmd = command.SwitchToPrevScreenCmd
+		default:
+			_, cmd = s.viewport.Update(msg)
 		}
 	}
 	return s, cmd
 }
 
 func (s *ReaderScreen) View() string {
-	viewableContent := strings.Join(s.contentToLines()[s.cursor:s.cursorEnd()], "\n")
-
 	return styles.DefaultBorderStyle.
 		Width(styles.SmallScreenWidth + 2).
 		Height(readerHeight).
-		Render(lipgloss.PlaceVertical(readerHeight, lipgloss.Left, viewableContent))
-}
-
-func (s *ReaderScreen) contentToLines() []string {
-	return strings.Split(s.content, "\n")
-}
-
-func (s *ReaderScreen) cursorEnd() int {
-	return min(len(s.contentToLines()), s.cursor+readerHeight+1)
+		Render(s.viewport.View())
 }
 
 // to fulfill FocusableModel interface
---
+++ ui/viewport/viewport.go
@@ -0,0 +1,75 @@
+package viewport
+
+import (
+	"strings"
+
+	"github.com/charmbracelet/bubbles/key"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"hostettler.dev/dnc/util"
+)
+
+type Viewport struct {
+	keymap  util.KeyMap
+	cursor  int
+	height  int
+	width   int
+	content string
+}
+
+func NewViewport(keymap util.KeyMap, height int, width int) *Viewport {
+	return &Viewport{keymap, 0, height, width, ""}
+}
+
+func (v *Viewport) Init() tea.Cmd {
+	return nil
+}
+
+func (v *Viewport) MoveCursor(offset int) {
+	newCursor := v.cursor + offset
+
+	if newCursor >= 0 && newCursor+v.height < len(v.contentToLines()) {
+		v.cursor = newCursor
+	}
+}
+
+func (v *Viewport) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, v.keymap.Up):
+			v.MoveCursor(-1)
+		case key.Matches(msg, v.keymap.Down, v.keymap.Enter):
+			v.MoveCursor(1)
+		}
+	}
+	return v, cmd
+}
+
+func (v *Viewport) View() string {
+	viewableContent := strings.Join(v.contentToLines()[v.cursor:v.cursorEnd()], "\n")
+
+	return lipgloss.NewStyle().
+		Width(v.width).
+		Height(v.height).
+		Render(lipgloss.Place(v.width, v.height, lipgloss.Left, lipgloss.Left, viewableContent))
+}
+
+func (v *Viewport) UpdateContent(content string) {
+	v.content = content
+}
+
+func (v *Viewport) Reset() {
+	v.cursor = 0
+	v.content = ""
+}
+
+func (v *Viewport) contentToLines() []string {
+	return strings.Split(v.content, "\n")
+}
+
+func (v *Viewport) cursorEnd() int {
+	return min(len(v.contentToLines()), v.cursor+v.height+1)
+}
